1e12cee7211955e419c0b09c5fb7f1351c2e7989
justins472 <40777462+justins472@users.noreply.github.com>
2018-09-15 18:22:01 -0400

    Update savacodes.py

--- savacodes.py
+++ savacodes.py
@@ -1,14 +1,21 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
-
-TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
-
-client = commands.Bot(command_prefix='?')
+import asyncio
+import time
 
 
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "?") #Initialise client bot
+
+
+@client.event 
+async def on_ready():
+    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
 
 @client.event
-async def on_ready():
-    print('Ready')
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
 
-client.run(TOKEN)
+client.run("token") #Replace token with your bots token
