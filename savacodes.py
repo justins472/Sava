4652158a50cedbb813bb96532220738be7a40462
justins472 <40777462+justins472@users.noreply.github.com>
2018-09-15 18:19:31 -0400

    Update savacodes.py

--- savacodes.py
+++ savacodes.py
@@ -5,9 +5,7 @@ TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
 
 client = commands.Bot(command_prefix='?')
 
-if(cmd == ${prefix}hello{
-    message.reply"Hello!"
-})
+
 
 @client.event
 async def on_ready():
