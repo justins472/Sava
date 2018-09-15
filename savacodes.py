9f6937fcebea2b14454bf918a98c6ae002d13b1d
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-09-15 16:55:35 -0500

    Add files via upload

---
+++ savacodes.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
+
+client = commands.Bot(command_prefix='?')
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+client.run(TOKEN)
