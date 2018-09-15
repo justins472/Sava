bf733b56a3bbb9b3b881de875cf34fe85f03a0c4
justins472 <40777462+justins472@users.noreply.github.com>
2018-09-15 18:26:09 -0400

    Update savacodes.py

--- savacodes.py
+++ savacodes.py
@@ -1,13 +1,14 @@
- import discord
- from discord.ext import commands
+import discord
+from discord.ext import commands
 
- TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
+TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
 
- client = commands.Bot(command_prefix='?')
+client = commands.Bot(command_prefix='?')
 
 
 
- @client.event
- async def on_ready():
-     print('Ready')
- client.run(TOKEN)
+@client.event
+async def on_ready():
+    print('Ready')
+
+client.run(TOKEN)
