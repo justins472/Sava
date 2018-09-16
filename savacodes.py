bddf4f540ae035f68a2fc656462e0d63f3491366
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-09-15 20:23:14 -0500

    Update savacodes.py

--- savacodes.py
+++ savacodes.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
 
 client = commands.Bot(command_prefix='?')
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -14,8 +15,17 @@ async def on_command_error(error, ctx):
     channel = ctx.message.channel
     if isinstance(error, commands.CommandNotFound):
         embed = discord.Embed(color=0xff0000)
-        embed.add_field(name='Unknown Error!', value='```There was an unknown error \n! We will report this to the staff team ASAP!```', inline=False)
+        embed.add_field(name='Unknown Error!', value='```There was an unknown error!\n We will report this to the staff team ASAP!```', inline=False)
         await client.send_message(channel, embed=embed)
+       
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(color=0xff00e6)
+    embed.set_author(name='SavaBot Help!')
+    embed.add_field(name='Current Status:', value='We are currently working on the bot Sorry!', inline=False)
+    await client.say(embed=embed)
+   
 
  
 client.run(TOKEN)
