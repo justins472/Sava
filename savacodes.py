2e6726a2ca2ab6b508b3040a7046f18b06195128
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-09-15 20:16:33 -0500

    Update savacodes.py

--- savacodes.py
+++ savacodes.py
@@ -5,10 +5,17 @@ TOKEN = 'NDkwNjIzMzk3NDU0MTUxNjgx.Dn8I-A.H7N23wPrBzDPErwYCImImxxuo1M'
 
 client = commands.Bot(command_prefix='?')
 
-
-
 @client.event
 async def on_ready():
-    print('Ready')
+ print('Ready')
 
+@client.event
+async def on_command_error(error, ctx):
+    channel = ctx.message.channel
+    if isinstance(error, commands.CommandNotFound):
+        embed = discord.Embed(color=0xff00e6)
+        embed.add_field(name='Unknown Error!', value='```There was an unknown error! We will report this to the staff team ASAP!', inline=False)
+        await client.say(embed=embed)
+
+ 
 client.run(TOKEN)
